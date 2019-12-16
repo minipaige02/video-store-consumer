dee555c5f8f33915c82f4020c15a529574bd3471
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-16 15:05:36 -0800

    successfully imported inventory[] and customers[] into App.js front end

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Library = () => {
+
+const Library = (props) => {
   return(
     <section>
-
       Library HERE!!!!
     </section>
 
