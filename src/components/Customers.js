ac4749c2a13204104d72ee51bdb34c8565324859
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-17 10:19:47 -0800

    merging

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -6,29 +6,18 @@ import './Customers.css';
 const Customers = (props) => {
 
   const listCustomers = props.customers.map((customerObj, i) => {
+    
     return <Customer key={i} {...customerObj} currCustomerCallback={props.currCustomerCallback}/>
   })
-
-  const noCustomers = (
-      <tr>
-        <th>No Customers</th>
-        <th>No Customers</th>
-        <th>No Customers</th>
-        <th>No Customers</th>
-        <th>No Customers</th>
-      </tr>
-    );
-
 
   
 
   return(
     <section>
-
       <table className="table table-striped">
         <thead className="table-header-row">
           <tr>
-            <th>Checking Out</th>
+            <th>CurrCustomer</th>
             <th>Id</th>
             <th>Name</th>
             <th>Account Credit</th>
@@ -37,7 +26,7 @@ const Customers = (props) => {
         </thead>
         
         <tbody>
-          {props.customers.length === 0 ? noCustomers:listCustomers}
+          {listCustomers}
         </tbody>
           
         
