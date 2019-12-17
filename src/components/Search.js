b77df5b31890e8733e378e2b2b89cb5ce1b07085
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-16 16:34:09 -0800

    external movie search API successful.  next up: print out those results with checkboxes to allow us to add to rental library

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-const Search = () => {
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      searchTerm: "",
+      error: "",
+      searchResults: [],
+    }
+  }
+
+  getInput = (event) => {
+    this.setState({ searchTerm: event.target.value});
+  }
+
+  sendAPI = (event) => {
+    event.preventDefault();
+
+    axios.get(`http://localhost:2999/movies?query=${this.state.searchTerm}`)
+    .then(response => {
+      this.setState({ searchTerm: "", searchResults: response.data });
+    })
+    .catch(error => {
+      this.setState({ error: error.message });
+    })
+
+  }
+
+
+  render(){
+
+    console.log(this.state.searchTerm);
+    console.log(this.state.searchResults);
+    
+    
   return(
-    <section>
+        <section>
+          <form onSubmit={this.sendAPI}>
+            <input type="text" value={this.state.searchTerm} placeholder="search term" onChange={this.getInput}/>
+            <input type="submit"/>
+          </form>
+        </section>
 
-      Search HERE!!!!
-    </section>
-
-  );
+      
+  )};
+   
+  
 };
 
 export default Search;
