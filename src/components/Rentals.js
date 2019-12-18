760313356166cbd2af74664c77bf64da949a6223
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-18 09:30:19 -0800

    Rentals overdues now shows up.  formatDate() now in Helpers.js

--- src/components/Rentals.js
+++ src/components/Rentals.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { formatDate } from './Helpers';
+
+
 
 class Rentals extends React.Component {
 
@@ -7,8 +10,41 @@ class Rentals extends React.Component {
     this.props.eraseAlertsCallback();
   }
 
+  showAllOverdues = (overdueRentals) => {
+    return ( overdueRentals.map((entry, i) => {
+      return (<tr key={i}>
+      <td>{entry.title}</td>
+      <td>{entry.name}</td>
+      <td>{formatDate(entry.checkout_date)}</td>
+      <td>{formatDate(entry.due_date)}</td>
+      <td>check in button here???</td>
+      </tr>)
+    })
+    );
+  }
+
   render() {
-    return("lol");
+    return(
+      <section>
+          <h3>Rental Library</h3>
+          <table className="table table-striped">
+            <thead className="table-header-row">
+              <tr>
+                <th>Movie</th>
+                <th>Customer</th>
+                <th>Checkout Date</th>
+                <th>Due Date</th>
+                <th>Check in</th>
+              </tr>
+            </thead>
+  
+            <tbody>
+              {this.showAllOverdues(this.props.overdueRentals)}
+            </tbody>
+          </table>
+        </section>
+
+    );
   }
 }
 
